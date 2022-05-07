58ee851a56b3f833c0de627e15b47d12c0d4f4d4
Sergio Campama <kaipi@copilot.money>
2022-05-07 00:14:01 -0400

    add missing async method for linux

--- Sources/APIBuilder/DefaultRequestExecutor.swift
+++ Sources/APIBuilder/DefaultRequestExecutor.swift
@@ -18,3 +18,28 @@ public class DefaultRequestExecutor: RequestExecutor {
     return Response(httpResponse: httpResponse, data: data)
   }
 }
+
+#if os(Linux)
+extension URLSession {
+  func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+    return try await withCheckedThrowingContinuation { continuation in
+      let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+          continuation.resume(throwing: error)
+          return
+        }
+        guard let response = response as? HTTPURLResponse else {
+          continuation.resume(throwing: StringError("invalid url response"))
+          return
+        }
+        guard let data = data else {
+          continuation.resume(throwing: StringError("missing response data"))
+          return
+        }
+        continuation.resume(returning: (data, response))
+      }
+      task.resume()
+    }
+  }
+}
+#endif  // os(Linux)
